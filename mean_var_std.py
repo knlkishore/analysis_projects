a49883b59e41257a9bd6946db55f0b71b061517e
knlkishore <62202743+knlkishore@users.noreply.github.com>
2022-06-02 00:45:46 +0530

    1

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 def calculate(arr):
-    print("---------------------------------------------1")
     #Checking if the passed list have 9 elements or not
     if len(arr) != 9:
         raise ValueError("List must contain nine numbers ")
 
+    #converting list to 3*3 numpy array
     A = np.array(arr)
     A = A.reshape(3,3)
     
@@ -16,14 +16,3 @@ def calculate(arr):
     'min': [A.min(axis=0).tolist(), A.min(axis=1).tolist(), A.min()],
     'sum': [A.sum(axis=0).tolist(), A.sum(axis=1).tolist(), A.sum()],
     }
-
-
-
-A = [0,1,2,3,4,5,6,7,8]
-Z =  calculate(A)
-print("---------------------------------------------")
-for i in Z:
-    print(Z[i])
-
-print("---------------------------------------------")
-
